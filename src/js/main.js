5eadd32e82bc9394c97196da30b8c15b123279c7
Goran Schyns <goran.schyns@student.hepl.be>
2019-06-15 15:28:11 +0100

    Ground, clouds and dino are moving

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,15 @@ const cloudsCoordinates = {
     'y' : 100,
 };
 
+const dinoCoordinates = {
+    'x' : 25,
+    'y' : 539,
+};
+
+const dinoSpriteCoordinates = {
+    'x' : 280,
+    'y' : 185,
+}
 
 const game = {
     'canvas' : document.getElementById('game'),
@@ -15,22 +24,47 @@ const game = {
     'spriteSrc' : "./resources/sprite.png",
     'sprite' : new Image(),
     init () {
-        this.ctx = this.canvas.getContext('2d'),
-        this.sprite.src = this.spriteSrc,
-        this.draw()
+        this.ctx = this.canvas.getContext('2d');
+        this.sprite.src = this.spriteSrc;
+        this.draw();
     },
     draw() {
-        this.sprite.addEventListener('load', ()=>(
-            this.ctx.drawImage(this.sprite, 0,361, 1000, 680, 0, 0, 1000, 680),
-            this.ctx.drawImage(this.sprite, 0, 0, 1348, 173, groundCoordinates.x, groundCoordinates.y, 1348, 173),
-            this.ctx.drawImage(this.sprite, 0, 1041, 1000, 162, cloudsCoordinates.x, cloudsCoordinates.y, 1041, 162),
-            this.ctx.drawImage(this.sprite, 280, 185, 120, 139, 25, 678-139, 120, 139)
-        ))
+        this.sprite.addEventListener('load', ()=> {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.drawImage(this.sprite, 0, 361, 1000, 680, 0, 0, 1000, 680);
+            this.ctx.drawImage(this.sprite, 0, 0, 1348, 173, groundCoordinates.x, groundCoordinates.y, 1348, 173);
+            this.ctx.drawImage(this.sprite, 0, 1041, 1000, 162, cloudsCoordinates.x, cloudsCoordinates.y, 1041, 162);
+            this.ctx.drawImage(this.sprite, dinoSpriteCoordinates.x, dinoSpriteCoordinates.y, 120, 139, dinoCoordinates.x, dinoCoordinates.y, 120, 139);
+        })
+        window.requestAnimationFrame(()=> {animation.move()});
     },
+};
+
+
+game.init()
+
+
+const animation = {
+    move(){
+        groundCoordinates.x = groundCoordinates.x - 4;
+        if (groundCoordinates.x <= -350){
+            groundCoordinates.x = 0;
+        }
+        cloudsCoordinates.x = cloudsCoordinates.x - 0.1;
+        if (cloudsCoordinates.x <= -900){
+            cloudsCoordinates.x = 900;
+        }
+        dinoSpriteCoordinates.x = dinoSpriteCoordinates.x + 120;
+        if (dinoSpriteCoordinates.x > 520) {
+            dinoSpriteCoordinates.x = 280;
+        }
+        game.init();
+    }
 };
 
 
 
 
-game.init()
+
+
 
